f714126a468ecb9051879d6f53b9c4d583e69f3c
Bart Stals <bartstals@MacBook-Pro-van-Bart.local>
2018-09-26 17:45:26 +0200

    material icons & navbar full width

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
+import { NavbarComponent } from './elements/navbar/navbar.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './elements/navbar/navbar.component';
 import { PagesModule } from './pages/pages.module';
 import { MaterialModule } from './shared/material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
